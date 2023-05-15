aa4d88570a6abea6c047b3d361f794bc84aa207d
mph7 <kawlost83783@gmail.com>
2023-05-15 10:13:12 -0300

    feat: make what is section responsive

--- src/components/DividedTextImage/DividedTextImage.jsx
+++ src/components/DividedTextImage/DividedTextImage.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import './DividedTextImage.css';
 
 const DividedTextImage = ({ isImgLeft, p, h1, imageSrc, button }) => {
-  return ( <div className="divided-text-image">
-    {isImgLeft && <div className="image-content">
-      <img src={imageSrc} alt="" className="divided-image"/>
-    </div>}
+  return ( <div className={`divided-text-image ${isImgLeft ? 'left-divided-image' : ''}`}>
     <div className="text-content">
       <h1 className="divided-h1">{h1}</h1>
       <p className="divided-p">{p}</p>
-      {button &&  button }
+      {button && button}
     </div>
-    {!isImgLeft && <div className="image-content">
+    <div className="image-content">
       <img src={imageSrc} alt="" className="divided-image"/>
-    </div>}
-    
+    </div>
+  
   </div> );
 };
 
