9d132da782eae1fd3d4d7be8eff6791b4d6d41ea
mph7 <kawlost83783@gmail.com>
2023-05-26 16:51:26 -0300

    feat: improve site design

--- src/container/PageHeader/PageHeader.jsx
+++ src/container/PageHeader/PageHeader.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import './PageHeader.css'
+import './PageHeader.css';
 
 const PageHeader = () => {
   return (
     <div className="page-header">
-    <div className="logo">
-      <img src="images/logo-unifsp-white.png" alt="unifsp logo" className="header-unifsp-logo"/>
-    </div>
+      <div className="logo">
+        <img src="images/logo-unifsp-white.png" alt="unifsp logo" className="header-unifsp-logo"/>
+      </div>
+      <a href="https://www.instagram.com/engenhariadeproducao.unifsp/" target="_blank">
+        <img src="assets/instagram-icon.png" alt="instagram icon"/>
+      </a>
     </div>
   );
 };
